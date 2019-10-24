9eab88961138201ec86bb02f180a2d861b0444aa
wesleymsmith <wmsmith@uci.edu>
2019-10-24 15:09:26 -0700

    added calculation of edge transition periods to allow computing MFPT

--- analysis_functions.py
+++ analysis_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import itertools
 
 def analyze_milestone1D_data(dataTable,windowMins,windowMaxs,verbose=False):
     winMins=np.array(windowMins)
@@ -14,6 +15,8 @@ def analyze_milestone1D_data(dataTable,windowMins,windowMaxs,verbose=False):
     xbins=np.sort(simData.X_Index.unique())
     nBins=len(xbins)
     escapeMat=np.zeros((nBins,nBins))
+    rVec=np.zeros(nBins-1)
+    tSum=0
     #iVal->escape matrix row index
     #xbin->window
     #cVal->place holder for bin index with indexing starting at 1
@@ -28,8 +31,16 @@ def analyze_milestone1D_data(dataTable,windowMins,windowMaxs,verbose=False):
             transInds=tCounts[0][1:] #first entry should always be for binT==0
             transCounts=tCounts[1][1:]
             cCount=np.sum(binC)
+            #generate escape matrix
             for iInd,Ind in enumerate(transInds):
                 escapeMat[iVal,Ind-1]=1.*transCounts[iInd]/cCount
+            runList=np.array([[int(j[0]),len(j)] for j in [list(g) for k,g in itertools.groupby(binT)]])
+            #generate R vector
+            for iRun,run in enumerate(runList[1:]):
+                if run[0]==0:
+                    #the bin edge index between bins i and j is min(i,j)
+                    rVec[np.min([runList[iRun,0]-1,xbin])]+=run[1]
+            tSum=tSum+np.sum(binC)
             if verbose:
                 print "--- --- ---"
                 print "escapeMatrix entry for window %g:"%xbin
@@ -50,4 +61,4 @@ def analyze_milestone1D_data(dataTable,windowMins,windowMaxs,verbose=False):
     outVec=np.array(outEig[1])[:,si[0]]
     outVec=outVec/np.sum(outVec)
     
-    return (escapeMat,outVec)
+    return (escapeMat,outVec,rVec/tSum)
