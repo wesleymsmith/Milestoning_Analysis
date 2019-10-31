d25e8d4c37942cdad1c69bacb652d9afb0a24a25
wesleymsmith <wmsmith@uci.edu>
2019-10-31 12:33:37 -0700

    fixed bugs related to cell and edge indexing in Compute_MFPT.m

--- Compute_MFPT.m
+++ Compute_MFPT.m
@@ -19,74 +19,89 @@ msgbox("Load window occupancy count data (should end in .T.dat)")
 [tFileName,tFileDir]=uigetfile()
 allT=load("-ascii",strcat(tFileDir,tFileName))
 
-%ncells = number of cells - the former and the last cell
-%this should be one less than the total number of entries
-%in the equilibrium cell distribution data file
-ncells=length(mu)-1;
-nmiles=ncells+1;
+%ncells = number of cells (milestone windows
+%nmiles = number of edges between cells (ncells-1)
+ncells=length(mu);
+nmiles=ncells-1;
 
 % compute Nij from Nij^a
 
+%since this is 1D the milestone (edge) between
+%cells i and j is equal to min(i,j)
+%Nij tracks transitions from edge i to edge j
+%these are found in the entry for cell min(i,j)
+%in allN, where column 1 is for i<j and column 2 is for i>j
 N=zeros(nmiles,nmiles);
-for k=1:ncells;
+for k=1:nmiles;
 N(k,k+1)=mu(k)*allN(k,1)/allT(k);
 N(k+1,k)=mu(k)*allN(k,2)/allT(k);
 end
 N
 % compute Ri from Ri^a
-
-R(1)=mu(1)*allR(1,1)/allT(1);
-R(nmiles)=mu(ncells)*allR(ncells,2)/allT(ncells);
-for j=2:nmiles-1;
-R(j)=mu(j-1)*allR(j-1,2)/allT(j-1)+mu(j)*allR(j,1)/allT(j);
+% We use the same convention as for Nij above
+% in allR, then entry for cell i contains the
+% timecount for edge i in the first column and
+% the timecount for edge i+1 in the second column
+R=zeros(nmiles)
+%j accumulate for right side cell edges
+%  *the right hand edge for cell j is edge j
+%k accumulate for left side cell edges
+%  *the left hand edge for cell k is edge k-1
+%Note j counts up from 1 to nmiles (number of edges)
+% while k counts down from ncells to 2
+for j=1:nmiles;
+  k=ncells-j+1
+  R(j)=R(j)+mu(j)*allR(j,2)/allT(j);
+  R(k-1)=R(k-1)+mu(k)*allR(k,1)/allT(k);
 end
 R
 % compute global Q
-
+%Qij = Nij / Ri
 Q=zeros(nmiles,nmiles);
 for k=1:nmiles-1;
 Q(k,k+1)=N(k,k+1)/R(k);
 Q(k+1,k)=N(k+1,k)/R(k+1);
 end
 
-a=Q;
-aa=a;
-na=size(a,1);
+%a=Q;
+%aa=a;
+%na=size(a,1);
 
 %% Reduce Q by eliminating zero lines and rows, when needed
 
-ic = 0;
-jkept=[];
-for j=1:na;
- if (sum((a(j,:))==zeros(1,na))) == na
-  aa (j-ic,:) = [];
-  ic = ic+1;
- else
-     jkept = [jkept j];   %% this array will keep track of the kept milestones in the old numeration
- end
-end
+%ic = 0;
+%jkept=[];
+%for j=1:na;
+% if (sum((a(j,:))==zeros(1,na))) == na
+%  aa (j-ic,:) = [];
+%  ic = ic+1;
+% else
+%     jkept = [jkept j];   %% this array will keep track of the kept milestones in the old numeration
+% end
+%end
 
-ic = 0;
-for j=1:na;
- if (sum((a(:,j))==zeros(na,1))) == na
-  aa (:,j-ic) = [];
-  ic = ic+1;
- end
-end
+%ic = 0;
+%for j=1:na;
+% if (sum((a(:,j))==zeros(na,1))) == na
+%  aa (:,j-ic) = [];
+%  ic = ic+1;
+% end
+%end
 
-b=aa;
-na=size(a)
-naa=size(aa)
+%b=aa;
+%na=size(a)
+%naa=size(aa)
 
 %% Rebuild diagonal terms, and eliminate cemetery state 
+aa=Q
 
  for j=1:size(aa);
    aa(j,j) = -sum(aa(j,:)); 
  end
 
- b=aa;
- aa(:,naa) = [];
- aa(naa,:) = [];
+% b=aa;
+% aa(:,naa) = [];
+% aa(naa,:) = [];
  
  nr=size(aa,1)
 
@@ -96,10 +111,12 @@ Qij=aa
 
  % take into account timestep & frames to have actual time
    % frq of writing frames
-        nfrq = 100;
+   %     nfrq = 100;
+   nfrq=inputdlg("Enter number of timesteps between frames"){1}
    % timestep 2 fs 
-     dt = nfrq*2*1e-15; %(2 fs in s)
- 
+   %  dt = nfrq*2*1e-15; %(2 fs in s)
+   dts=str2double(inputdlg("Enter simulated timestep duration (in seconds)"){1})
+   dt=nfrq*dts
   % solving linear system equation (5) Maragliano et al. JCTC 2009 
 t1=aa\(-ones(nr,1));
 
